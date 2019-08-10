b094de766a9bbace6cd99a166f2aa25f5d6d5c74
returntears <returntears@outlook.com>
2019-08-11 00:02:00 +0800

    Javascript Code

--- esutils/utils.js
+++ esutils/utils.js
@@ -1,11 +1,11 @@
 // better JS Code
-function Utils() {
+function Utils(params) {
     const anagrams = str => {
         if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
         return str.split('').reduce((acc, letter, i) => {
             anagrams(str.slice(0, i) + str.slice(i + 1)).map(val => acc.push(letter + val));
             return acc;
         }, []);
-    }
-
+    };
+    return anagrams(params)
 }
