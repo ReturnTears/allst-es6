07a65ea8053afbe48727c9f37f2b45fb43168d0f
returntears <returntears@outlook.com>
2019-09-19 00:36:28 +0800

    Javascript Code

---
+++ es6base/es6adva.js
@@ -0,0 +1,12 @@
+// ES6高级部分
+function getValue(condition) {
+    if (condition) {
+        var value = "blue";
+        console.log(value)
+        return value;
+    } else {
+        console.log(value)
+        return null;
+    }
+    console.log(value)
+}
