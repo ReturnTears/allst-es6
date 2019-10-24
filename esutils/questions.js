6e30824b63564aefc6a4665727bc44181bf8876b
returntears <returntears@outlook.com>
2019-10-25 00:04:21 +0800

    面试问题

--- esutils/questions.js
+++ esutils/questions.js
@@ -10,3 +10,9 @@ function foo() {
 foo();
 console.log(typeof a); // undefined  变量a仅在foo()作用域内部声明，而在作用域外部中不可用。
 console.log(typeof b); // number 0  b是意外创建的全局变量
+
+// No.2 数组长度属性
+const clothes = ['jacket', 't-shirt'];
+clothes.length = 0;
+// 数组对象的length属性具有特殊行为: 减小length属性的值有删除当前数组元素的副作用
+console.log(clothes[0]);    // undefined
