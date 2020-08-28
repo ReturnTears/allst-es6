3f2a197c3368ffa3730ae1a8c3e6465e9eb8d10c
JUNN <returntears@outlook.com>
2020-08-28 18:16:13 +0800

    ArrayBuffer

---
+++ es6base/arrayBuf.js
@@ -0,0 +1,16 @@
+// ArrayBuffer
+/*
+ArrayBuffer 对象用来表示通用的、固定长度的原始二进制数据缓冲区。
+ArrayBuffer 不能直接操作，而是要通过类型数组对象或DataView对象来操作，
+它们会将缓冲区中的数据表示为特定的格式，并通过这些格式来读写缓冲区的内容。
+语法：
+new ArrayBuffer(length)
+length：表示创建的ArrayBuffer的大小，单位为字节。
+如果 length 大于 Number.MAX_SAFE_INTEGER（>= 253）或为负数，则抛出一个 RangeError异常。
+*/
+const buffer = new ArrayBuffer(8);
+console.log(buffer.byteLength);
+console.log(ArrayBuffer.length);
+class MyArrayBuffer extends ArrayBuffer {
+  static get [Symbol.species]() { return ArrayBuffer; }
+}
