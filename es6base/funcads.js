59517efbab85507056b83b185bd3e2664db1b182
returntears <returntears@outlook.com>
2020-09-20 11:22:38 +0800

    Function

--- es6base/funcads.js
+++ es6base/funcads.js
@@ -101,9 +101,55 @@ function f(x = 1, y) {
 // console.log(f(10));
 
 // 有默认值的解构参数,以通过解构赋值为参数赋值
-function fm([x, y] = [1, 2], {z: z} = {z: 3}) {
+function fm([x, y] = [1, 2], { z: z } = { z: 3 }) {
     return x + y + z;
 }
 //console.log(fm());
 
+
+/**
+ * Function 构造函数 创建一个新的Function对象
+ * 每个函数实际上都是一个Function对象。
+ * 语法：
+ * new Function ([arg1[, arg2[, ...argN]],] functionBody)
+ * 参数：
+ * arg1, arg2, ... argN被函数使用的参数的名称必须是合法命名的。
+ * 参数名称是一个有效的JavaScript标识符的字符串，或者一个用逗号分隔的有效字符串的列表。
+ * functionBody一个含有包括函数定义的JavaScript语句的字符串。
+ * 
+ * 使用Function构造器生成的Function对象是在函数创建时解析的。这比你使用函数声明或者函数表达式(function)并在你的代码中调用更为低效，
+ * 因为使用后者创建的函数是跟其他代码一起解析的。所有被传递到构造函数中的参数，都将被视为将被创建的函数的参数，并且是相同的标示符名称和传递顺序。
+ * 以调用函数的方式调用Function的构造函数 (不是用new关键字) 跟以构造函数来调用是一样的.
+ * 
+ * Function的属性和方法
+ * 全局的Function对象没有自己的属性和方法, 但是, 因为它本身也是函数，所以它也会通过原型链从Function.prototype上继承部分属性和方法。
+ */
+var fcn = new Function('a', 'b', 'return a + b');
+// console.log(fcn(2, 6));
+// console.log(fcn.caller);
+// console.log(fcn.name);
+// console.log(fcn.displayName);
+// console.log(fcn.length);
+
+/**
+ * 函数构造器和函数声明的区别:
+ * 使用Function构造器创建的函数不会为其创建上下文及闭包; 他们总是在全球范围内创建。
+ * 在运行它们时，它们只能访问自己的局部变量和全局变量，而不能调用Function构造函数的范围。这与用eval函数表达式的代码不同。
+ */
+var x = 10;
+function createFunction1() {
+    var x = 20;
+    return new Function('x', 'return x'); // this |x| refers global |x|
+}
+function createFunction2() {
+    var x = 20;
+    function f() {
+        return x; // this |x| refers local |x| above
+    }
+    return f;
+}
+var f1 = createFunction1();
+console.log(f1(x));
+//var f2 = createFunction2();
+//console.log(f2());
 
