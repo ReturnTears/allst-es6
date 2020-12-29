bb317ba92bb5fcd8a50b064e62be4f026db46b24
returntears <returntears@outlook.com>
2020-12-30 00:41:55 +0800

    函数式编程

---
+++ es6base/funcprogramming.js
@@ -0,0 +1,31 @@
+// 函数式编程
+var add = function(x, y) { return x + y };
+var multiply = function(x, y) { return x * y };
+
+var flock_a = 4;
+var flock_b = 2;
+var flock_c = 0;
+
+var result = add(multiply(flock_b, add(flock_a, flock_c)), multiply(flock_a, flock_b));
+// console.log(result); // node .\funcprogramming.js  > 16
+
+// 结合律（assosiative）
+add(add(x, y), z) == add(x, add(y, z));
+
+// 交换律（commutative）
+add(x, y) == add(y, x);
+
+// 同一律（identity）
+add(x, 0) == x;
+
+// 分配律（distributive）
+multiply(x, add(y,z)) == add(multiply(x, y), multiply(x, z));
+
+// 原有代码
+add(multiply(flock_b, add(flock_a, flock_c)), multiply(flock_a, flock_b));
+
+// 应用同一律，去掉多余的加法操作（add(flock_a, flock_c) == flock_a）
+add(multiply(flock_b, flock_a), multiply(flock_a, flock_b));
+
+// 再应用分配律
+multiply(flock_b, add(flock_a, flock_a));
