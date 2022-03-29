b6fd7927e351055fc32742c9a55aa01cae030224
returntears <returntears@outlook.com>
2022-03-29 22:10:37 +0800

    lodash

--- esutils/lodash.js
+++ esutils/lodash.js
@@ -2,17 +2,25 @@
 var _ = require('lodash');
 
 // 数组相关函数
-// 1、chunk()
+// 1、chunk(array, size=1) 根据size参数将数组分组，size是每一组的长度。如果数组不能均分，最后一组就会包含剩下的元素
 const _chunk1 = _.chunk(['a', 'b', 'c', 'd'], 2); // => [['a', 'b'], ['c', 'd']] 
 console.log(_chunk1)
 
 const _chunk2 = _.chunk(['a', 'b', 'c', 'd'], 3); // => [['a', 'b', 'c'], ['d']]
 console.log(_chunk2)
 
+// 创建一个数组，其中删除所有false值
 const _compact = _.compact([0, 1, false, 2, '', 3]); // => [1, 2, 3]
 console.log(_compact)
 
+// 创建array与任何其他数组和/或值连接的新数组。
+let array = [1];
+let other = _.concat(array, 2, [3], [[4]]);
+console.log(other)
 
+// array使用SameValueZero相等性比较创建未包含在其他给定数组中的值数组。结果值的顺序和引用由第一个数组确定。
+let diff = _.difference([2, 1], [2, 3]);
+console.log(diff)
 
 // 实用函数
 var elements = _.attempt(function(selector) {
